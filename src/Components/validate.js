967af9425b32d06b75e62da12929f03df79405de
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-02 16:03:57 +0200

    correct function

--- src/Components/validate.js
+++ src/Components/validate.js
@@ -2,12 +2,18 @@ export function validate(values) {
     const validatePESEL = (pesel) => {
         let peselCorrectLength = 11;
         let toNumber = Number(pesel);
-        return !(pesel.length !== peselCorrectLength || isNaN(toNumber));
+        if(toNumber) {
+            return !(pesel.length !== peselCorrectLength || isNaN(toNumber));
+        }
+        return false;
     };
     const validateNIP = (nip) => {
         let nipCorrectLength = 10;
         let toNumber = Number(nip);
-        return !(nip.length !== nipCorrectLength || isNaN(toNumber));
+        if(toNumber) {
+            return !(nip.length !== nipCorrectLength || isNaN(toNumber));
+        }
+        return false;
     };
     const errorDataMessages = {};
     const { nip, pesel } = values;
